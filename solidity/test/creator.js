5d2c018f48ec8cbcbc552cafe1e8e17f4a901083
hwavy <haruki4dev@gmail.com>
2021-10-24 02:05:43 +0900

    hotfix

--- solidity/test/creator.js
+++ solidity/test/creator.js
@@ -4,32 +4,32 @@
 /*globals assert*/
 /*globals before*/
 
-const Creator = artifacts.require("Creator");
+const Creator = artifacts.require('Creator');
 
 /*
  * uncomment accounts to access the test accounts made available by the
  * Ethereum client
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
-contract("Creator", function (/* accounts */) {
-  it("should assert true", async function () {
-    await Creator.deployed();
-    return assert.isTrue(true);
-  });
-
+contract('Creator', function (/* accounts */) {
   let creator;
   let ballotId = 4294967295;
 
-  before(async function () {
-    creator = await Creator.new();
+  before(async () => {
+    creator = await Creator.deployed();
   });
 
-  it("should be possible to get the address of the voting contract by ballotid", () => {
-    creator
-      .createBallot(1000000, 0, 3, ballotId, "Title", 0)
-      .then(async function (votingAddress) {
-        let result = await creator.getAddress(ballotId);
-        assert.equal(result, votingAddress);
-      });
+  it('can get the voting address from the ballotid', () => {
+    creator.createBallot(1000000, 0, 3, ballotId, 'Title', 0).then(async (votingAddress) => {
+      let result = await creator.getAddress(ballotId);
+      assert.equal(result, votingAddress);
+    });
+  });
+
+  it('can get the ballotid from the voting address', () => {
+    creator.createBallot(1000000, 0, 3, ballotId, 'Title', 0).then(async (votingAddress) => {
+      let result = await creator.getBallotId(votingAddress);
+      assert.equal(result, ballotId);
+    });
   });
 });
