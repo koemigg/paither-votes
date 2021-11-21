80efbf8c5167c5236cc543e8baf2e5261004afbc
hwavy <haruki4dev@gmail.com>
2021-11-21 17:16:17 +0900

    feat: init and set creator

--- client/src/pages/Vote.js
+++ client/src/pages/Vote.js
@@ -1,38 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ethers } from 'ethers'
 import { Button, Table, Space, Divider, Input, Layout, Typography } from 'antd'
 import { Header } from './Header'
-import SimpleStorageContract from '../contracts/SimpleStorage.json'
+import CreatorArtifacts from '../contracts/Creator.json'
+import VotingArtifacts from '../contracts/Voting.json'
 
 const { Content, Footer } = Layout
 const { Title } = Typography
+const { Search } = Input
 
 function Main() {
   const [storageValue, setStorageValue] = React.useState('0')
+  const [voting, setVoting] = useState()
   const [contract, setContract] = React.useState()
   const [accounts, setAccounts] = React.useState('No account connected.')
 
   const isMetaMaskConnected = () => accounts && accounts.length > 0
 
-  const onClickConnect = async () => {
+  const onClickInit = async () => {
     const ethereum = window.ethereum
     const newAccounts = await ethereum.request({
       method: 'eth_requestAccounts'
     })
     setAccounts(newAccounts)
+    getCreator()
   }
 
-  const onClickGetContract = async () => {
+  const getCreator = async () => {
     try {
       if (isMetaMaskConnected()) {
         // Get the contract instance.
         const provider = new ethers.providers.Web3Provider(window.ethereum)
         // const { networkId } = await provider.getNetwork();
-        const deployedNetwork = SimpleStorageContract.networks[5777]
+        const deployedNetwork = CreatorArtifacts.networks[5777]
         const signer = provider.getSigner(0)
-        const instance = new ethers.Contract(deployedNetwork.address, SimpleStorageContract.abi, signer)
+        const instance = new ethers.Contract(deployedNetwork.address, CreatorArtifacts.abi, signer)
         setContract(instance)
-        console.log('Setting up contract.')
+        console.log('Setting up creator contract.')
       } else {
         console.log('Metamask is not connected.')
       }
@@ -46,6 +50,8 @@ function Main() {
     const response = await contract.get()
     setStorageValue(response.toString())
   }
+
+  const onClickLoad = async () => {}
 
   const columns = [
     {
@@ -79,7 +85,6 @@ function Main() {
   ]
 
   const onSearch = (value) => console.log(value)
-  const { Search } = Input
 
   return (
     <Layout className="layout">
@@ -96,14 +101,8 @@ function Main() {
               Account: <i>{accounts}</i>
             </div>
             <Space direction="horizontal" size="large" align="center" split={<Divider type="vertical" />}>
-              <Button type="primary" onClick={onClickConnect}>
+              <Button type="primary" onClick={onClickInit}>
                 Connect
-              </Button>
-              <Button type="primary" onClick={onClickGetContract}>
-                Get Contract
-              </Button>
-              <Button type="primary" onClick={onClickExcute}>
-                Excute
               </Button>
             </Space>
           </Space>
@@ -127,21 +126,23 @@ function Main() {
           <br />
           <Space size="large" align="top" split={<Divider type="vertical" />}>
             <Space direction="vertical" size="small" align="center">
-              <h2>Register</h2>
-              <div>Register to vote.</div>
-              <Button type="primary">Register</Button>
-            </Space>
-            <Space direction="vertical" size="small" align="center">
               <h2>Load Ballot</h2>
               <div>Load Ballot.</div>
               <Search
                 style={{ width: 300 }}
                 placeholder="input Ballot ID"
                 allowClear
-                enterButton="Load"
+                enterButton="onClickLoad"
                 size="middle"
                 onSearch={onSearch}
               />
+            </Space>
+            <Space direction="vertical" size="small" align="center">
+              <h2>Register</h2>
+              <div>Register to vote.</div>
+              <Input style={{ width: 300 }} placeholder="E-mail Adderess" allowClear />
+              <Input style={{ width: 300 }} placeholder="Your BSU student/employee ID" allowClear />
+              <Button type="primary">Register</Button>
             </Space>
             <Space direction="vertical" size="small" align="center">
               <h2>Vote</h2>
