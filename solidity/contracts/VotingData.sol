ccf220fa646c99f09345c23a6e12a1ca5bd4e266
hwavy <haruki4dev@gmail.com>
2022-01-17 01:36:03 +0900

    Uploading ciphertext at 1024 bits, storing keys in contracts

--- solidity/contracts/VotingData.sol
+++ solidity/contracts/VotingData.sol
@@ -8,7 +8,41 @@ contract VotingData is Voting{
 
     using SafeMath for uint256;
 
+    event newBigInteger1024(string name, uint x1, uint x256, uint x512, uint x768);
+    event newBigInteger2048(string name, uint x1, uint x256, uint x512, uint x768, uint x1024, uint x1280, uint x1536, uint x1792);
+
     constructor (uint32 _timeLimit, uint8 _ballotType, uint8 _voteLimit, uint32 _ballotId, string memory _title, uint8 _whiteListType, bytes32[] memory _candidates, bytes32[] memory _whiteStuff, uint256[] memory _publicKey, address _owner) Voting(_timeLimit, _ballotType, _voteLimit, _ballotId, _title, _whiteListType, _candidates, _whiteStuff, _publicKey, _owner){}
+
+    /// @notice Stores a huge integer up to 1024.
+    /// @dev value = x1 + x256 * 2 ** 256 + x512 * 2 ** 512 + x768 * 2 ** 768
+    struct BigInteger1024 {
+        uint x1;
+        uint x256;
+        uint x512;
+        uint x768;
+    }
+
+    /// @notice Stores a huge integer up to 2048.
+    /// @dev value = x1 + x256 * 2 ** 256 + ... + x1792 * 2 ** 1792
+    struct BigInteger2048 {
+        uint x1;
+        uint x256;
+        uint x512;
+        uint x768;
+        uint x1024;
+        uint x1280;
+        uint x1536;
+        uint x1792;
+    }
+
+    BigInteger1024 n;
+    BigInteger2048 g;
+    BigInteger1024 lambda;
+    BigInteger1024 mu;
+
+    BigInteger2048 debugNumber = BigInteger2048(1, 0, 0, 0, 0, 0, 0, 0);
+
+    /*
 
     function getVotes() public view returns (uint256[] memory){
         uint256[] memory allVotes;
@@ -21,7 +55,6 @@ contract VotingData is Voting{
         return allVotes;
         // return c.votesReceived[keccak256(abi.encode(bytes32ToString(c.candidateList[0])))];
     }
-
 
     /// @param _votes Encrypted number of votes want to add.
     /// @param _email Voter's E-mail address.
@@ -40,46 +73,50 @@ contract VotingData is Voting{
             for(uint i = 0; i < _candidates.length; i++) {
             bytes32 _hash = c.candidateHash[_candidates[i]];
             if (validCandidate(_hash) == false) revert('This is a non-existent option/candidate name.');
-            c.votesReceived[_hash] = addition(c.votesReceived[_hash], _votes[i]);
+            // c.votesReceived[_hash] = addition(c.votesReceived[_hash], _votes[i]);
         }
     }
 
-    function getPublicKey() public view returns (uint256[2] memory) {
-        return [b.publicKey.n, b.publicKey.g];
+    */
+
+    function setPublicKey(uint256[] memory _n, uint256[] memory _g) public onlyOwner {
+        n = BigInteger1024(_n[0], _n[1], _n[2], _n[3]);
+        g = BigInteger2048(_g[0], _g[1], _g[2], _g[3], _g[4], _g[5], _g[6], _g[7]);
+        emit newBigInteger1024('set n', _n[0], _n[1], _n[2], _n[3]);
+        emit newBigInteger2048('set g', _g[0], _g[1], _g[2], _g[3], _g[4], _g[5], _g[6], _g[7]);
+    }
+
+    function getPublicKey() public view returns (uint256[12] memory) {
+        return [n.x1, n.x256, n.x512, n.x768, g.x1, g.x256, g.x512, g.x768, g.x1024, g.x1280, g.x1536, g.x1792];
     }
 
     /// @notice Parameters for the key to decrypt the number of votes. Can only be called after the voting period is over and by the author of the vote.
-    /// @param _privateKey Private key for decrypting votes. _privateKey[0]: lambda, _privateKey[1]: mu
-    function setPrivateKey(uint256[] memory _privateKey) public onlyOwner {
+    /// @param _lambda lambda of private key for decrypting votes. lambda[0]: reminder of value, lambda[1]: reminder of quotient, lambda[2]: quotient of quotient
+    /// @param _mu mu of private key for decrypting votes. mu[0]: reminder of value, mu[1]: reminder of quotient, mu[2]: quotient of quotient
+    function setPrivateKey(uint256[] memory _lambda, uint256[] memory _mu) public onlyOwner {
         // For Debug mode
         // if (checkTimelimit()) revert ('Voting is now open.'); 
-        b.privateKey.lambda = _privateKey[0];
-        b.privateKey.mu = _privateKey[1];
+        lambda = BigInteger1024(_lambda[0], _lambda[1], _lambda[2], _lambda[3]);
+        mu = BigInteger1024(_mu[0], _mu[1], _mu[2], _mu[3]);
+        emit newBigInteger1024('set lambda', _lambda[0], _lambda[1], _lambda[2], _lambda[3]);
+        emit newBigInteger1024('set mu', _mu[0], _mu[1], _mu[2], _mu[3]);
     }
 
-    function getPrivateKey() public view returns (uint256[2] memory) {
+    function getPrivateKey() public view returns (uint256[8] memory) {
         // For Debug mode
         // if (checkTimelimit()) revert ('Voting is now open.');
-        if (b.privateKey.lambda == 0 && b.privateKey.mu == 0) revert ('No private key uploaded yet.');
-        return [b.privateKey.lambda, b.privateKey.mu];
+        if (lambda.x1 == 0 && lambda.x256 == 0 && lambda.x512 == 0 && lambda.x768 == 0) revert ('No private key uploaded yet.');
+        return [lambda.x1, lambda.x256, lambda.x512, lambda.x768, mu.x1, mu.x256, mu.x512, mu.x768];
     }
 
-    function addition(uint256 c1, uint256 c2) private view returns (uint256){
-        return (c1.mul(c2)).mod(b.publicKey.n ** 2);
-    }
-    
-    uint256 debugNumber = 1;
-
-    function addNumber (uint256 value) public {
-        debugNumber = addition(debugNumber, value);
+    function setNumber (uint256[] memory values_) public {
+        debugNumber = BigInteger2048(values_[0], values_[1], values_[2], values_[3], values_[4], values_[5], values_[6], values_[7]);
+        emit newBigInteger2048('set debugNumber', values_[0], values_[1], values_[2], values_[3], values_[4], values_[5], values_[6], values_[7]);
     }
 
-    function getNumber () public view returns (uint256){
-        return debugNumber;
+    function getNumber () public view returns (uint256[8] memory){
+        return [debugNumber.x1, debugNumber.x256, debugNumber.x512, debugNumber.x768, debugNumber.x1024, debugNumber.x1280, debugNumber.x1536, debugNumber.x1792];
     }
 
-    function setNumber (uint256 value) public {
-        debugNumber = value;
-    }
 }
 
--- solidity/test/VotingData.js
+++ solidity/test/VotingData.js
@@ -11,10 +11,9 @@ contract('VotingData', function (accounts) {
   const ballotId = Math.floor(Math.random() * 4294967295);
   const choices = ['Truffle', 'Ganache', 'Creme brulee'];
 
-  const SOLIDITY_MAX_INT = BigInt(Math.pow(2, 256) - 1);
+  // const SOLIDITY_MAX_INT = BigInt(Math.pow(2, 256)) - 1n;
   // console.log('BigInt(SOLIDITY_MAX_INT): ' + SOLIDITY_MAX_INT);
-  const bitLength = 128;
-
+  const bitLength = 1024;
   // web3StringToBytes32
   const W3STB32 = (str) => {
     let result = ethers.utils.hexlify(ethers.utils.toUtf8Bytes(str));
@@ -47,82 +46,98 @@ contract('VotingData', function (accounts) {
     return encodeBytes;
   };
 
-  it(`0. 鍵の長さ: ${bitLength} bit`, function () {
+  function zeroPadding(NUM, LEN) {
+    return (Array(LEN).join('0') + NUM).slice(-LEN);
+  }
+
+  const BigIntToSolBigInt = (value, bitLength = 1024, size = 256) => {
+    console.assert(bitlengthCount(value) <= bitLength, `値が${bitLength}bit以上`);
+    console.assert(bitLength % size == 0, `bitLengthが${size}で割り切れる`);
+    const iterations = bitLength / size;
+    const result = [];
+    const value_bin_str = zeroPadding(value.toString(2), bitLength);
+    let offset = 0;
+    for (let i = 0; i < iterations; i++) {
+      const item = Number(value_bin_str.slice(offset, offset + size)) == 0 ? 0n : BigInt('0b' + value_bin_str.slice(offset, offset + size));
+      result.unshift(item);
+      offset += size;
+    }
+    return result;
+  };
+
+  const SolBigIntToBigInt = (values, size = 256) => {
+    let result = 0n;
+    values.forEach((value, index) => {
+      const multiplier = BigInt(size) * BigInt(index);
+      result += BigInt(value) * 2n ** multiplier;
+    });
+    return BigInt(result);
+  };
+
+  const bitlengthCount = (value) => {
+    return value.toString(2).length;
+  };
+
+  it(`鍵の長さ: ${bitLength} bit`, () => {
     assert.isTrue(true);
   });
 
-  it('1. 暗号文をアップロードせずに鍵だけをアップロードする (加算は行わない)', async function () {
+  it('公開鍵、秘密鍵、投票結果をコントラクトに保存する', async () => {
+    // Setup
     const { publicKey, privateKey } = await paillier.generateRandomKeys(bitLength);
     const n = publicKey.n;
     const g = publicKey.g;
     const mu = privateKey.mu;
     const lambda = privateKey.lambda;
-    const votingData = await VotingData.new(1000000, 0, 3, ballotId, 'Title', 0, W3SATB32(choices), [], [n, g], owner);
-    await votingData.setPrivateKey([lambda, mu]);
-    const m = 12345n;
-    const c = publicKey.encrypt(m);
-    const dec1 = privateKey.decrypt(c);
-    const publicKeyParameter = await votingData.getPublicKey();
-    const publicKey_voter = new paillier.PublicKey(BigInt(publicKeyParameter[0]), BigInt(publicKeyParameter[1]));
-    const privateKeyParameter = await votingData.getPrivateKey();
-    const privateKey_voter = new paillier.PrivateKey(BigInt(privateKeyParameter[0]), BigInt(privateKeyParameter[1]), publicKey_voter);
-    const dec2 = privateKey_voter.decrypt(c);
-
-    // console.log('n        : ' + n);
-    // console.log('pkSeed[0]: ' + pkSeed[0]);
-    // console.log('g        : ' + g);
-    // console.log('pkSeed[1]: ' + pkSeed[1]);
-    // console.log('dec1: ' + dec1);
-    // console.log('dec2: ' + dec2);
-    // console.log('publicKey: ');
-    // console.dir(publicKey, { depth: null });
-    // console.log('pk       : ');
-    // console.log('privateKey: ');
-    // console.dir(pk, { depth: null });
-    // console.dir(privateKey, { depth: null });
-    // console.log('sk       : ');
-    // console.dir(sk, { depth: null });
-
-    expect(publicKey).to.deep.equal(publicKey_voter, '最初に作成した公開鍵とコントラクトから取得した公開鍵は等しい');
-    expect(lambda).equal(BigInt(privateKeyParameter[0]), '最初に作成した秘密鍵とコントラクトから取得した秘密鍵は等しい(λ)');
-    expect(mu).equal(BigInt(privateKeyParameter[1]), '最初に作成した秘密鍵とコントラクトから取得した秘密鍵は等しい(μ)');
-    expect(dec1.toString()).to.equal(m.toString(), '復号した暗号文は平文に等しい (ローカルの鍵使用)');
-    expect(dec2.toString()).to.equal(m.toString(), '復号した暗号文は平文に等しい (コントラクトから取得した鍵使用)');
-  });
-
-  it('2. 公開鍵、秘密鍵、投票結果をコントラクトに保存する', async function () {
-    const { publicKey, privateKey } = await paillier.generateRandomKeys(bitLength);
-    const n = publicKey.n;
-    const g = publicKey.g;
-    const mu = privateKey.mu;
-    const lambda = privateKey.lambda;
-    const votingData = await VotingData.new(1000000, 0, 3, ballotId, 'Title', 0, W3SATB32(choices), [], [n, g], owner);
+    const votingData = await VotingData.new(1000000, 0, 3, ballotId, 'Title', 0, W3SATB32(choices), [], [0, 0], owner);
     let actuall_result_votes = 0n;
 
+    // Set Public key
+    const g_converted = BigIntToSolBigInt(g, 2048);
+    await votingData.setPublicKey(BigIntToSolBigInt(n), g_converted);
+
     // [Vote Phase] Voter 1
-    const publicKeyParam_voter = await votingData.getPublicKey();
-    const publicKey_voter = new paillier.PublicKey(BigInt(publicKeyParam_voter[0]), BigInt(publicKeyParam_voter[1]));
+    const publicKey_voter = await (async () => {
+      const arr = await votingData.getPublicKey();
+      return new paillier.PublicKey(SolBigIntToBigInt([arr[0], arr[1], arr[2], arr[3]]), SolBigIntToBigInt([arr[4], arr[5], arr[6], arr[7], arr[8], arr[9], arr[10], arr[11]]));
+    })();
+
+    expect(publicKey).to.deep.equal(publicKey_voter, 'コントラクトを経由して公開鍵を取得しても値は変わらない');
     const m = 100n;
     const c1 = publicKey_voter.encrypt(m);
-    const encrypted_now_votes_1 = await votingData.getNumber();
-    await votingData.setNumber(publicKey_voter.addition(c1, BigInt(encrypted_now_votes_1)));
+    const encrypted_now_votes_1 = await (async () => {
+      const arr = await votingData.getNumber();
+      return SolBigIntToBigInt([arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6], arr[7]]);
+    })();
+    const encrypted_sum1 = publicKey_voter.addition(c1, BigInt(encrypted_now_votes_1));
+    await votingData.setNumber(BigIntToSolBigInt(encrypted_sum1, 2048));
     actuall_result_votes += m;
 
     // [Vote Phase] Voter 2
     const c2 = publicKey_voter.encrypt(m);
-    const encrypted_now_votes_2 = await votingData.getNumber();
-    await votingData.setNumber(publicKey_voter.addition(c2, BigInt(encrypted_now_votes_2)));
+    const encrypted_now_votes_2 = await (async () => {
+      const arr = await votingData.getNumber();
+      return SolBigIntToBigInt([arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6], arr[7]]);
+    })();
+    const encrypted_sum2 = publicKey_voter.addition(c2, BigInt(encrypted_now_votes_2));
+    await votingData.setNumber(BigIntToSolBigInt(encrypted_sum2, 2048));
     actuall_result_votes += m;
 
     // [Count Phase] Creator
-    await votingData.setPrivateKey([lambda, mu]);
+    await votingData.setPrivateKey(BigIntToSolBigInt(lambda), BigIntToSolBigInt(mu));
 
     // [Count Phase] Voter
-    const encrypted_result_votes = await votingData.getNumber();
-    const privateKeyParam_voter = await votingData.getPrivateKey();
-    const privateKey_voter = new paillier.PrivateKey(BigInt(privateKeyParam_voter[0]), BigInt(privateKeyParam_voter[1]), publicKey_voter);
+    const encrypted_result_votes = await (async () => {
+      const arr = await votingData.getNumber();
+      return SolBigIntToBigInt([arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6], arr[7]]);
+    })();
+    const privateKey_voter = await (async () => {
+      const arr = await votingData.getPrivateKey();
+      return new paillier.PrivateKey(SolBigIntToBigInt([arr[0], arr[1], arr[2], arr[3]]), SolBigIntToBigInt([arr[4], arr[5], arr[6], arr[7]]), publicKey_voter);
+    })();
+
     const result_votes = privateKey_voter.decrypt(BigInt(encrypted_result_votes));
 
-    expect(result_votes.toString()).to.equal(actuall_result_votes.toString(), 'アップロードされた暗号文を再び取得しても値は変わらない');
+    expect(actuall_result_votes.toString()).to.equal(result_votes.toString(), 'コントラクトを経由して暗号文を取得しても値は変わらない');
   });
 });
